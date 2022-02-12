622290d6140f12a7d54ea6222aa93c97943d2a6a
as585-s <as585@myscc.ca>
2022-02-12 08:28:52 -0500

    Corrected the content-list-component-ts file

--- A_Saeed_BakingWorld/src/app/content-list/content-list.component.ts
+++ A_Saeed_BakingWorld/src/app/content-list/content-list.component.ts
@@ -7,18 +7,18 @@ import { Content } from '../helper-files/content-interface';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
-  @Input() content?: Content;
-  contentArray: Content[];
+//  @Input() content?: Content;
+  contentList: Content[];
   
   constructor() {
-    this.contentArray = [{
+    this.contentList = [{
       id: 0,
       title: "Homemade Bread",
       description: "Home baked bread",
       creator: "June Xie",
       imgURL: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-bread-horizontal-1547759080.jpg?crop=0.671xw:1.00xh;0.0801xw,0&resize=980:*",
       type: "Whole Wheat",
-      tags: ['Delish Bread']},
+      tags: ['Delish Bread', "whole wheat bread"]},
     {
       id: 1,
       title: "Homemade Pizza",
@@ -26,7 +26,7 @@ export class ContentListComponent implements OnInit {
       creator: "Makinze Gore",
       imgURL: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-homemade-pizza-horizontal-1542312378.png?crop=1.00xw:1.00xh;0,0&resize=980:*",
       type: "All purpose flour",
-      tags: ['Delish Pizza']},
+      tags: ['Delish Pizza', "home made pizza"]},
     {
       id: 2,
       title: "Simple Whole Wheat Bread",
@@ -34,7 +34,7 @@ export class ContentListComponent implements OnInit {
       creator: "Jenny Jones",
       imgURL: "https://www.jennycancook.com/wp-content/uploads/2016/04/Simple_Whole_Wheat_Bread.jpg",
       type: "Whole Wheat",
-      tags: ['Delish Bread']},
+      tags: ['Delish Bread', "home baked bread"]},
     {
       id: 3,
       title: "Baked Chicken",
@@ -42,7 +42,7 @@ export class ContentListComponent implements OnInit {
       creator: "Sienna Livermore",
       imgURL: "https://life-in-the-lofthouse.com/wp-content/uploads/2015/11/Chicken-Noodle-Casserole_new.jpg",
       type: "Baked chicken",
-      tags: ['Delish Baked Chicken']},
+      tags: ['Delish Chicken', "Baked chicken"]},
     {
       id: 4,
       title: "Baked Potatoes",
@@ -50,7 +50,7 @@ export class ContentListComponent implements OnInit {
       creator: "Jeanine and Jack",
       imgURL: "https://cdn.loveandlemons.com/wp-content/uploads/2020/01/baked-potato-580x580.jpg",
       type: "Baked potatoes",
-      tags: ['Love and Lemons']
+      tags: ['Love and Lemons', "Baked potatoes"]
     },
     {
       id: 5,
@@ -59,7 +59,7 @@ export class ContentListComponent implements OnInit {
       creator: "Cassie Marshall",
       imgURL: "https://thekitchencommunity.org/wp-content/uploads/2021/07/Roast-Beef-Curry.jpg",
       type: "Roast beef",
-      tags: ['The Kitchen Community']
+      tags: ['The Kitchen Community', "Roast Beef"]
     }
   ]
    }
