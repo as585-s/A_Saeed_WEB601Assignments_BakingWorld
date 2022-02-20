ee84186a7f6346787c9602c5651b82fc21722dd7
as585-s <as585@myscc.ca>
2022-02-20 15:34:01 -0500

    made it work up to underlining the title but it also underlines the tags

--- A_Saeed_BakingWorld/src/app/hover-affect.directive.ts
+++ A_Saeed_BakingWorld/src/app/hover-affect.directive.ts
@@ -6,30 +6,39 @@ import { Content } from './helper-files/content-interface';
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective implements OnInit {
-  style?: string; 
- // @Input() title?: string;
-
+ // style?: string; 
+  @Input() title?: string;
+  @Input() tags?: string;
+ 
+ 
   constructor(private elm: ElementRef) {
     elm.nativeElement.style.textdecoration = "none";
+    
     //elm.nativeElement.style.cursor = "pointer";
    }
    ngOnInit(): void {
        
    }
+
    @HostListener('mouseover') mouseover() {
-    console.log(this.elm.nativeElement);
+     if(this.title="title"){
     this.elm.nativeElement.style = "text-decoration: underline";
-    /*
-    this.hoverFunction();
-        console.log("test function");
-    */
+    //this.hoverFunction();
+     } else if (this.tags="tags"){
+      this.elm.nativeElement.style = "font-weight: 700px";
+     }
    }
 
-/*
+   @HostListener('mouseleave') mouseleave() {
+    this.elm.nativeElement.style = "text-decoration: none";
+    //this.hoverFunction();
+         
+   }
+
+   /*
    private hoverFunction(): void {
-   //   this.elm.nativeElement.style.pointer = "underline";
-   this.style = "underline";
-      console.log("hoverFunction works");
+     //this.elm.nativeElement.style = "text-decoration: underline";
+
    }
 */
 }
