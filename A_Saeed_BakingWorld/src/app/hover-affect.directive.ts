9c68d593c86f33f7bb919742f162e9aa2b7259a3
as585-s <as585@myscc.ca>
2022-02-20 17:35:32 -0500

    Cleaned  up the code

--- A_Saeed_BakingWorld/src/app/hover-affect.directive.ts
+++ A_Saeed_BakingWorld/src/app/hover-affect.directive.ts
@@ -1,39 +1,26 @@
 import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { Content } from './helper-files/content-interface';
-
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective implements OnInit {
-
- // @Input() title?: string;
-//  @Input() tags?: string;
- 
   constructor(private elm: ElementRef) {
     elm.nativeElement.style.textdecoration = "none";
-    
-    //elm.nativeElement.style.cursor = "pointer";
-   }
-   ngOnInit(): void {
-       
-   }
+        //elm.nativeElement.style.cursor = "pointer";
+  }
 
-   @HostListener('mouseover') mouseOver() {
-     console.log(this.elm.nativeElement);
+  ngOnInit(): void {}
+
+  @HostListener('mouseover') mouseOver() {
     if(this.elm.nativeElement.className == "title"){
       this.elm.nativeElement.style = "text-decoration: underline";
-    
-    } else if(this.elm.nativeElement.className == "tag"){
+    } 
+    else if(this.elm.nativeElement.className == "tag"){
       this.elm.nativeElement.style = "font-weight: 700";
-
     }
-   }
-
-   @HostListener('mouseleave') mouseLeave() {
-    this.elm.nativeElement.style = "text-decoration: none";
-    
-   }
-
   }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.elm.nativeElement.style = "";
+  }
+}
