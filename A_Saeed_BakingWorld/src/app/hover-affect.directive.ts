bbf6624e598e72876cc482c7998c55c46a821c77
as585-s <as585@myscc.ca>
2022-02-20 17:21:23 -0500

    completed assignment 4

--- A_Saeed_BakingWorld/src/app/hover-affect.directive.ts
+++ A_Saeed_BakingWorld/src/app/hover-affect.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Content } from './helper-files/content-interface';
 
 
@@ -6,10 +7,9 @@ import { Content } from './helper-files/content-interface';
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective implements OnInit {
- // style?: string; 
-  @Input() title?: string;
-  @Input() tags?: string;
- 
+
+ // @Input() title?: string;
+//  @Input() tags?: string;
  
   constructor(private elm: ElementRef) {
     elm.nativeElement.style.textdecoration = "none";
@@ -20,25 +20,20 @@ export class HoverAffectDirective implements OnInit {
        
    }
 
-   @HostListener('mouseover') mouseover() {
-     if(this.title="title"){
-    this.elm.nativeElement.style = "text-decoration: underline";
-    //this.hoverFunction();
-     } else if (this.tags="tags"){
-      this.elm.nativeElement.style = "font-weight: 700px";
-     }
+   @HostListener('mouseover') mouseOver() {
+     console.log(this.elm.nativeElement);
+    if(this.elm.nativeElement.className == "title"){
+      this.elm.nativeElement.style = "text-decoration: underline";
+    
+    } else if(this.elm.nativeElement.className == "tag"){
+      this.elm.nativeElement.style = "font-weight: 700";
+
+    }
    }
 
-   @HostListener('mouseleave') mouseleave() {
+   @HostListener('mouseleave') mouseLeave() {
     this.elm.nativeElement.style = "text-decoration: none";
-    //this.hoverFunction();
-         
+    
    }
 
-   /*
-   private hoverFunction(): void {
-     //this.elm.nativeElement.style = "text-decoration: underline";
-
-   }
-*/
-}
+  }
