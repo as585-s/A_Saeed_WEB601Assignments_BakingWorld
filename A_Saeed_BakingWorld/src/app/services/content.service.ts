dcd6e954c8db6bd2db70ac8ad8b073deb398aaa0
as585-s <as585@myscc.ca>
2022-03-13 12:44:06 -0400

    added function to service to retrieve content array from contentDb

--- A_Saeed_BakingWorld/src/app/services/content.service.ts
+++ A_Saeed_BakingWorld/src/app/services/content.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {contentList} from '../helper-files/contentDb';
+import {Content} from '../helper-files/content-interface';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  constructor() {}
+  
+    getContent(): Content[]{
+      return contentList;
+    }
+
+    //function that will retrieve the content array from the contentDb file
+    //and return it using the observalbe pattern.
+    getContentObs() : Observable<Content[]> {
+      return of(contentList);
+    }
+  
 }
+function getContent() {
+  throw new Error('Function not implemented.');
+}
+
