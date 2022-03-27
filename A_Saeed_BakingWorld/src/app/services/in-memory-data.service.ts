016cd85ca5ba27b12bf1de0f7a85844dec879461
as585-s <as585@myscc.ca>
2022-03-27 13:28:53 -0400

    added createDb method  to in-memory-data-service

--- A_Saeed_BakingWorld/src/app/services/in-memory-data.service.ts
+++ A_Saeed_BakingWorld/src/app/services/in-memory-data.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Content } from '../helper-files/content-interface';
+import { contentList} from '../helper-files/contentDb';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb(){
+    //setting it to the value of our array of content
+    const content : Content[] = contentList;
+    return {content};
+  }
 
   constructor() { }
 }
