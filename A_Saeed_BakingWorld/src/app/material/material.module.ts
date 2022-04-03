cf72305357f307f4766486c4b6a91dd7c925c66c
as585-s <as585@myscc.ca>
2022-04-03 11:26:44 -0400

    added material theme to title search

--- A_Saeed_BakingWorld/src/app/material/material.module.ts
+++ A_Saeed_BakingWorld/src/app/material/material.module.ts
@@ -1,14 +1,16 @@
+import { NgModule, Input } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { NgModule } from '@angular/core';
-
-
 
 const MaterialComponents = [
+  MatFormFieldModule,
+  MatInputModule,
   MatButtonModule
 ]
 
 @NgModule({
-    imports: [MaterialComponents ],
+    imports: [MaterialComponents],
     exports: [MaterialComponents]
 })
 export class MaterialModule { }
