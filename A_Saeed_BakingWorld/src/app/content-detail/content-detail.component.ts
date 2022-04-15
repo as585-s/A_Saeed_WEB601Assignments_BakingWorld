95d6b9acb575cf74c054946634e8786b6bb7cde0
as585-s <as585@myscc.ca>
2022-04-15 18:24:30 -0400

    updated content-detail.component.ts

--- A_Saeed_BakingWorld/src/app/content-detail/content-detail.component.ts
+++ A_Saeed_BakingWorld/src/app/content-detail/content-detail.component.ts
@@ -2,6 +2,7 @@ import { ContentService } from './../services/content.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Content } from '../helper-files/content-interface';
+import { MessageService } from '../services/message.service';
 
 
 @Component({
@@ -14,14 +15,15 @@ export class ContentDetailComponent implements OnInit {
   id?: number;
   contentItem?: Content;
 
-  constructor(private route: ActivatedRoute, private contentService: ContentService) { }
+  constructor (private messageService: MessageService, private mService: ContentService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.id = +(params.get('id') ?? "0"); //uses the + unary operator
-      this.contentService.getSingleContent(this.id).subscribe(
-        (c) => {
-          this.contentItem = c;
+      this.mService.getSingleContent(this.id).subscribe(
+        (contentItem) => {
+          this.contentItem = contentItem;
+          this.messageService.add(`Content at id ${contentItem.id} was retrieved, with a title of ${contentItem.title}`)
         });
     });
   }
