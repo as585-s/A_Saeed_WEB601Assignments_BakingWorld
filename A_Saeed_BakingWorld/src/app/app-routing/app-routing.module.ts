62c5163c5edc930daf1aabb056c5ad964be3e2c3
as585-s <as585@myscc.ca>
2022-04-15 17:51:15 -0400

    edited app-routing.module.ts and app.component.html according to sample solution

--- A_Saeed_BakingWorld/src/app/app-routing/app-routing.module.ts
+++ A_Saeed_BakingWorld/src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { ContentDetailComponent } from './../content-detail/content-detail.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from '@angular/router';
 import { ContentListComponent } from '../content-list/content-list.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
@@ -9,23 +8,27 @@ import { PageNotFoundComponent } from '../page-not-found/page-not-found.componen
 
 
 const routes: Routes = [
-  { path: "content/:id",
-  component: ContentDetailComponent
+  { path: "",
+    redirectTo: "/list",
+    pathMatch: "full"
   },
   {
-    path: "content",
-    component: ContentListComponent,
+    path: "list",
+    component: ContentListComponent
   },
   {
-    path: "**",
-    component: PageNotFoundComponent,
+    path: "list/:id",
+    component: ContentDetailComponent
   },
+ // {
+ //   path: "**",
+ //   component: PageNotFoundComponent,
+//  },
  ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     [RouterModule.forRoot(routes)]
     
   ],
